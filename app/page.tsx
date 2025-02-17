bbccdb9baf6e105dbaa40666dfa323df91ba8fd2
mintthiha <mintthiha@gmail.com>
2025-02-17 10:26:18 -0500

    Add project Setup. Closes #1

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <>
+      <h1> To Do App! </h1>
+      <Button> This is a Shadcn Button!</Button>
+    </>
+    
+  );
+}
