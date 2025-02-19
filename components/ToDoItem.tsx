016e6aa5e9abc651da38bf087555f67d09cd9a02
mintthiha <mintthiha@gmail.com>
2025-02-18 20:28:05 -0500

    Move Note inside same div as task div

--- components/ToDoItem.tsx
+++ components/ToDoItem.tsx
@@ -7,10 +7,10 @@ interface ToDoItemProps {
   onComplete: () => void;
 }
 
-export function ToDoItem({ task, onDelete, onComplete}: ToDoItemProps) {
+export function ToDoItem({ task, onDelete, onComplete }: ToDoItemProps) {
   return (
-    <>
-    <div className="flex items-center justify-between p-3 bg-gray-100 rounded-md">
+    <div className="flex flex-col p-3 bg-gray-100 rounded-md">
+      <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
           <Checkbox checked={task.completed} onCheckedChange={onComplete} />
           <span className={task.completed ? "line-through text-gray-500" : "text-black"}>
@@ -18,14 +18,12 @@ export function ToDoItem({ task, onDelete, onComplete}: ToDoItemProps) {
           </span>
         </div>
 
-        <DropDownTaskMenu onDelete={onDelete}></DropDownTaskMenu>
-        
+        <DropDownTaskMenu onDelete={onDelete} />
       </div>
-        {task.note && (
-          <div className="flex items-center justify-between p-3 bg-gray-100 rounded-md">
-            <p className="mt-2 text-sm text-gray-600 pl-8">{task.note}</p>
-          </div>
-        )}
-    </>
+
+      {task.note && (
+        <p className="mt-2 text-sm text-gray-600 pl-8">{task.note}</p>
+      )}
+    </div>
   );
 }
