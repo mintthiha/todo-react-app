ab2e2d2f00e5e18e513adcd5d9ba5a0946c014ec
mintthiha <mintthiha@gmail.com>
2025-02-19 16:28:43 -0500

    Fix Dark mode colours. Closes #8

--- components/ToDo/ToDoItem.tsx
+++ components/ToDo/ToDoItem.tsx
@@ -9,17 +9,19 @@ interface ToDoItemProps {
 
 export function ToDoItem({ task, onDelete, onComplete }: ToDoItemProps) {
   return (
-    <div className="flex flex-col p-3 bg-gray-100 rounded-md">
+    <div className="flex flex-col p-3 rounded-md bg-gray-100 dark:bg-gray-800">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
         <Checkbox checked={task.completed} onCheckedChange={onComplete} />
-          <span className={task.completed ? "line-through text-gray-500" : "text-black"}>{task.text}</span>
+          <span className={task.completed ? "line-through text-gray-500" : "text-gray-900 dark:text-gray-100"}>
+            {task.text}
+          </span>
         </div>
 
         <DropDownTaskMenu onDelete={onDelete} />
       </div>
+      {task.note && <p className={task.completed ? "mt-2 text-sm text-gray-600 pl-8 line-through" : "mt-2 text-sm text-gray-600 dark:text-gray-300 pl-8"}>{task.note}</p>}
 
-      {task.note && <p className={task.completed ? "mt-2 text-sm text-gray-600 pl-8 line-through" : "mt-2 text-sm text-gray-600 pl-8"}>{task.note}</p>}
     </div>
   );
 }
