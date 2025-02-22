f7562a512bf793ed4f4285ce1c1957334908e0d6
mintthiha <mintthiha@gmail.com>
2025-02-21 23:47:20 -0500

    Readd implementation for drag/drop for lists. Update README

--- README.md
+++ README.md
@@ -2,7 +2,24 @@
 
 ### Description
 
+This is a simple todo app built with **NextJS**, **Shadcn**, and **Swapy**. It is a simple web application that allows you to add and delete todos. It also allows you to add and delete task lists while saving your data to local storage, meaning on refresh, your data is still there.
+
 ### How To Run
+
+In order to run the project, you need to have Node.js installed. Next, you can run the project using:
+
+```bash
+npm install
+npm run dev
+```
+
+For production, you can use:
+
+```bash
+npm install
+npm run build
+npm run start
+```
 
 ### How to Deploy
 
@@ -20,4 +37,9 @@ Add link here when the time comes.
 ##### Why NextJS?
 
 So initially I tried creating the project using only React, With _create-react-app_, but I ran into issues regarding conflicting dependencies. I then went on to try Vite and React, but I also faced issues, but this time with **Tailwind**. Since I want to use **shadcn**, I decided to now use **NextJS**.
+
+##### Issues with Swapy
+
+Swapy is a great library for creating drag and drop divs with nice animations. However, it is still very buggy and the documentation is not the best. I tried to use it for the tasks themselves, but I kept running into issues regarding the order of the tasks after dragging them, even the state of it. I feel like I have spent too much time trying to figure out how to use it for the tasks, so for now it will be only on the task lists themselves. I also tried using Swapy for both the lists and the tasks within them, but Swapy would get confused and allowed swapping of lists and tasks, which does not make sense. 
+
 
--- components/TaskListManager.tsx
+++ components/TaskListManager.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import TaskList from "@/components/ToDo/ToDoList";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { customToast } from "./ui/customToast";
-// import { createSwapy, Swapy } from 'swapy';
+import { createSwapy, Swapy } from 'swapy';
 import TaskListMangerSideBar from "./TaskListManagerSideBar";
 
 export default function TaskListManager() {
@@ -14,9 +14,9 @@ export default function TaskListManager() {
     []
   );
   const [newListTitle, setNewListTitle] = useState("");
-  // const swapyRef = useRef<Swapy | null>(null)
-  // const containerRef = useRef<HTMLDivElement>(null);
-  // const [taskOrder, setTaskOrder] = useLocalStorage<string[]>("taskOrder", []);
+  const swapyRef = useRef<Swapy | null>(null)
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [taskOrder, setTaskOrder] = useLocalStorage<string[]>("taskOrder", []);
 
   const addTaskList = () => {
     if (newListTitle.trim() === "") {
@@ -45,53 +45,53 @@ export default function TaskListManager() {
     }
   };
 
-  // useEffect(() => {
-  //   if (!containerRef.current || swapyRef.current) return;
+  useEffect(() => {
+    if (!containerRef.current || swapyRef.current) return;
   
-  //   swapyRef.current = createSwapy(containerRef.current, {
-  //     animation: "dynamic",
-  //     swapMode: "drop",
-  //     autoScrollOnDrag: true,
-  //     dragAxis: "both"
-  //   });
+    swapyRef.current = createSwapy(containerRef.current, {
+      animation: "dynamic",
+      swapMode: "drop",
+      autoScrollOnDrag: true,
+      dragAxis: "both"
+    });
   
-  //   if (taskOrder.length > 0) {
-  //     const orderedTasks = taskOrder
-  //       .map((id) => taskLists.find((task) => task.id === id))
-  //       .filter(Boolean) as { id: string; title: string }[];
+    if (taskOrder.length > 0) {
+      const orderedTasks = taskOrder
+        .map((id) => taskLists.find((task) => task.id === id))
+        .filter(Boolean) as { id: string; title: string }[];
   
-  //     setTaskLists(orderedTasks);
-  //   }
+      setTaskLists(orderedTasks);
+    }
   
-  //   return () => {
-  //     swapyRef.current?.destroy();
-  //   };
+    return () => {
+      swapyRef.current?.destroy();
+    };
     
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   
-  // useEffect(() => {
-  //   if (!swapyRef.current) return;
+  useEffect(() => {
+    if (!swapyRef.current) return;
   
-  //   swapyRef.current.onSwapEnd((event) => {
-  //     const newOrder = event.slotItemMap.asArray.map(({ item }) => item);
-  //     console.log("New Order of Task IDs:", newOrder);
-  //     setTaskOrder(newOrder);
-  //   });
+    swapyRef.current.onSwapEnd((event) => {
+      const newOrder = event.slotItemMap.asArray.map(({ item }) => item);
+      console.log("New Order of Task IDs:", newOrder);
+      setTaskOrder(newOrder);
+    });
   
-  //   swapyRef.current.update();
-  // }, [taskLists, setTaskOrder]);  
+    swapyRef.current.update();
+  }, [taskLists, setTaskOrder]);  
 
   return (
     <div className="flex flex-col lg:flex-row h-screen">
       <TaskListMangerSideBar newListTitle={newListTitle} setNewListTitle={setNewListTitle} addTaskList={addTaskList} />
 
-      <div className="w-full lg:w-4/5 bg-secondary p-4 overflow-y-auto" id="swapy-container">
+      <div className="w-full lg:w-4/5 bg-secondary p-4 overflow-y-auto" id="swapy-container" ref={containerRef}>
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {taskLists.map((list) => (
             <div key={list.id} data-swapy-slot={list.id} className="relative">
               <div data-swapy-item={list.id}>
-                {/* <div className="absolute top-2 left-2 cursor-grab text-2xl leading-none text-gray-400" data-swapy-handle> ⋮⋮ </div> */}
+                <div className="absolute top-2 left-2 cursor-grab text-2xl leading-none text-gray-400" data-swapy-handle> ⋮⋮ </div>
                 <TaskList storageKey={`todo-list-${list.id}`} title={list.title} />
                 
                 <Button
